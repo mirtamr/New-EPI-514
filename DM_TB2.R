bda5cd778099f553d383abb68bd1b8dc5243c158
Aileen Andrade <aileenandrade@Aileens-MacBook-Pro.local>
2024-05-06 18:31:47 -0700

    issues with cervical cancer yes only

--- DM_TB2.R
+++ DM_TB2.R
@@ -1,28 +1,40 @@
 #############################################################################
 # NAME: DATA MANAGEMENT
 
-BRFSS <- read.csv("/Users/mirtamaravilla/Documents/UW Courses/Spring Qtr 2024/EPI 514 /EPI 514 Research/cleanbrfss.csv") 
-
-
+#BRFSSclean <- read.csv("/Users/mirtamaravilla/Documents/UW Courses/Spring Qtr 2024/EPI 514 /EPI 514 Research/cleanbrfss.csv") 
+BRFSSclean <- read.csv("/Users/aileenandrade/Desktop/Spring Quarter/Epi 514/BRFSS project/Data/cleanbrfss.csv") 
+View(BRFSSclean)
 #libraries
 library(epiR)
 
 #Prevalence of Cervical cancer 
 
-prevtable <- table(BRFSS$Hlthdiscrim_bin.f, BRFSS$CervScrnEver.f, dnn=c("Discrimination","Cervical Cancer Screening"))
+prevtable <- table(BRFSSclean$Hlthdiscrim_bin.f, BRFSSclean$CervScrnEver.f, dnn=c("Discrimination","Cervical Cancer Screening"))
 
 prop.table(prevtable)
 
-table(BRFSS$Hlthdiscrim_bin.f, BRFSS$CervScrnEver.f)
+table(BRFSSclean$Hlthdiscrim_bin.f, BRFSSclean$CervScrnEver.f)
 
 #Unadjusted PR (NO COVARIATES)
 
 #tables for cervical cancer screening 
-unadj <- with(BRFSS, table(Hlthdiscrim_bin.f, CervScrnEver.f))
+unadj <- with(BRFSSclean, table(Hlthdiscrim_bin.f, CervScrnEver.f))
 
 unadj
 
 epi.2by2(unadj, method = "cross.sectional")
+
+
+
+
+######################### Adjusted PRs ########################################
+
+cerv_yes <- BRFSSclean$CervScrnEver=="yes"
+
+#Race
+
+RceCervAdj <-table(BRFSSclean$Hlthdiscrim_bin.f, BRFSSclean$cerv_yes, BRFSSclean$race)
+
 
 
 
